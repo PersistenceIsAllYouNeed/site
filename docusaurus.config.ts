d8b09fbbee0986aabb308c740f0a5dd35172a13d
nz <zhouchao0630@gmail.com>
2025-03-10 16:42:37 -0400

    test with Prem

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -69,7 +69,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'Nick\'s Workbench',
+      title: 'Prem\'s Workbench',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
